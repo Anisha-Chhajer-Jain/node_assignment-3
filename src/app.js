0dd7cd5219ddfc31512471651fe8460359d36521
Anisha Chhajer <anishachhajer16@gmail.com>
2026-06-04 22:56:52 +0530

    added app.js file and code needed

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 
-// const express = require('express');
-// const noteRouter = require('./routes/note.route.js');
-// const app = express();
+const express = require('express');
+const noteRouter = require('./routes/note.route.js');
+const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// app.use('/api/notes',noteRouter);
+app.use('/api/notes',noteRouter);
 
-// app.use((req,res)=>{
-//     res.status(404).json({message:"Route not found"});
-// })
+app.use((req,res)=>{
+    res.status(404).json({message:"Route not found"});
+})
 
-// module.exports = app;
+module.exports = app;
