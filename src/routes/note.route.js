cdc7798740dfe84230e7ec31d8d49778152aee1f
Anisha Chhajer <anishachhajer16@gmail.com>
2026-06-04 23:17:12 +0530

    route file added note.route.js

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -1,54 +1,54 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-const {
-  createNote,
-  multipleNotes,
-  getAllNotes,
-  getNotesById,
-  UpdateById,
-  UpdateFieldId,
-  deleteById,
-  deleteMulti,
-  searchTitle,
-  searchContent,
-  searchAll,
-  filterSort,
-  filterPaginate,
-  sortPaginate,
-  searchFilter,
-  searchSortPaginate,
-  filterSortPaginate,
-  queryNotes
-} = require("../controllers/note.controller");
+// const {
+//   createNote,
+//   multipleNotes,
+//   getAllNotes,
+//   getNotesById,
+//   UpdateById,
+//   UpdateFieldId,
+//   deleteById,
+//   deleteMulti,
+//   searchTitle,
+//   searchContent,
+//   searchAll,
+//   filterSort,
+//   filterPaginate,
+//   sortPaginate,
+//   searchFilter,
+//   searchSortPaginate,
+//   filterSortPaginate,
+//   queryNotes
+// } = require("../controllers/note.controller");
 
-router.post("/", createNote);
-router.post("/bulk", multipleNotes);
-router.get("/query", queryNotes);
-
-
-router.get("/search", searchTitle);
-router.get("/search/content", searchContent);
-router.get("/search/all", searchAll);
+// router.post("/", createNote);
+// router.post("/bulk", multipleNotes);
+// router.get("/query", queryNotes);
 
 
-router.get("/filter-sort", filterSort);
-router.get("/filter-paginate", filterPaginate);
-router.get("/sort-paginate", sortPaginate);
-router.get("/search-filter", searchFilter);
-router.get("/search-sort-paginate", searchSortPaginate);
-router.get("/filter-sort-paginate", filterSortPaginate);
+// router.get("/search", searchTitle);
+// router.get("/search/content", searchContent);
+// router.get("/search/all", searchAll);
 
 
-router.get("/", getAllNotes);
-router.get("/:id", getNotesById);
+// router.get("/filter-sort", filterSort);
+// router.get("/filter-paginate", filterPaginate);
+// router.get("/sort-paginate", sortPaginate);
+// router.get("/search-filter", searchFilter);
+// router.get("/search-sort-paginate", searchSortPaginate);
+// router.get("/filter-sort-paginate", filterSortPaginate);
 
 
-router.put("/:id", UpdateById);
-router.patch("/:id", UpdateFieldId);
+// router.get("/", getAllNotes);
+// router.get("/:id", getNotesById);
 
 
-router.delete("/bulk", deleteMulti);
-router.delete("/:id", deleteById);
+// router.put("/:id", UpdateById);
+// router.patch("/:id", UpdateFieldId);
 
-module.exports = router;
+
+// router.delete("/bulk", deleteMulti);
+// router.delete("/:id", deleteById);
+
+// module.exports = router;
